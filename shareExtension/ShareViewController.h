c4bd2e7b26af5ce33c8b287f4a96f89e9599b7f6
zhangkn <929118967@qq.com>
2019-01-25 15:40:21 +0800

    薪资shareExtension

---
+++ shareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  shareExtension
+//
+//  Created by mac on 2019/1/25.
+//  Copyright © 2019 kunnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
